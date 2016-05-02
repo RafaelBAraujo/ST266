587dbd0098d5e6c3c92c459589145f4c6610a4e0
TheBeeGod <vitor_artonio@hotmail.com>
2016-05-02 08:01:04 -0300

    PixelCalc 1.0
    Primeiras implementações da calculadora

---
+++ Vitor/PixelCalc/PixelCalc/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace PixelCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botao_1 = new System.Windows.Forms.Button();
+            this.botao_2 = new System.Windows.Forms.Button();
+            this.botao_3 = new System.Windows.Forms.Button();
+            this.botao_4 = new System.Windows.Forms.Button();
+            this.botao_5 = new System.Windows.Forms.Button();
+            this.botao_6 = new System.Windows.Forms.Button();
+            this.botao_7 = new System.Windows.Forms.Button();
+            this.botao_8 = new System.Windows.Forms.Button();
+            this.botao_9 = new System.Windows.Forms.Button();
+            this.botao_0 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.botao_apagar = new System.Windows.Forms.Button();
+            this.botao_limpar = new System.Windows.Forms.Button();
+            this.botao_virgula = new System.Windows.Forms.Button();
+            this.botao_somar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // botao_1
+            // 
+            this.botao_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao_1.Location = new System.Drawing.Point(61, 166);
+            this.botao_1.Name = "botao_1";
+            this.botao_1.Size = new System.Drawing.Size(75, 23);
+            this.botao_1.TabIndex = 0;
+            this.botao_1.TabStop = false;
+            this.botao_1.Text = "1";
+            this.botao_1.UseVisualStyleBackColor = true;
+            this.botao_1.Click += new System.EventHandler(this.botao_1_Click);
+            // 
+            // botao_2
+            // 
+            this.botao_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao_2.Location = new System.Drawing.Point(133, 166);
+            this.botao_2.Name = "botao_2";
+            this.botao_2.Size = new System.Drawing.Size(75, 23);
+            this.botao_2.TabIndex = 1;
+            this.botao_2.TabStop = false;
+            this.botao_2.Text = "2";
+            this.botao_2.UseVisualStyleBackColor = true;
+            this.botao_2.Click += new System.EventHandler(this.botao_2_Click);
+            // 
+            // botao_3
+            // 
+            this.botao_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao_3.Location = new System.Drawing.Point(205, 166);
+            this.botao_3.Name = "botao_3";
+            this.botao_3.Size = new System.Drawing.Size(75, 23);
+            this.botao_3.TabIndex = 2;
+            this.botao_3.TabStop = false;
+            this.botao_3.Text = "3";
+            this.botao_3.UseVisualStyleBackColor = true;
+            this.botao_3.Click += new System.EventHandler(this.botao_3_Click);
+            // 
+            // botao_4
+            // 
+            this.botao_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao_4.Location = new System.Drawing.Point(61, 146);
+            this.botao_4.Name = "botao_4";
+            this.botao_4.Size = new System.Drawing.Size(75, 23);
+            this.botao_4.TabIndex = 3;
+            this.botao_4.TabStop = false;
+            this.botao_4.Text = "4";
+            this.botao_4.UseVisualStyleBackColor = true;
+            this.botao_4.Click += new System.EventHandler(this.botao_4_Click);
+            // 
+            // botao_5
+            // 
+            this.botao_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao_5.Location = new System.Drawing.Point(133, 146);
+            this.botao_5.Name = "botao_5";
+            this.botao_5.Size = new System.Drawing.Size(75, 23);
+            this.botao_5.TabIndex = 4;
+            this.botao_5.TabStop = false;
+            this.botao_5.Text = "5";
+            this.botao_5.UseVisualStyleBackColor = true;
+            this.botao_5.Click += new System.EventHandler(this.botao_5_Click);
+            // 
+            // botao_6
+            // 
+            this.botao_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao_6.Location = new System.Drawing.Point(205, 146);
+            this.botao_6.Name = "botao_6";
+            this.botao_6.Size = new System.Drawing.Size(75, 23);
+            this.botao_6.TabIndex = 5;
+            this.botao_6.TabStop = false;
+            this.botao_6.Text = "6";
+            this.botao_6.UseVisualStyleBackColor = true;
+            this.botao_6.Click += new System.EventHandler(this.botao_6_Click);
+            // 
+            // botao_7
+            // 
+            this.botao_7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao_7.Location = new System.Drawing.Point(61, 126);
+            this.botao_7.Name = "botao_7";
+            this.botao_7.Size = new System.Drawing.Size(75, 23);
+            this.botao_7.TabIndex = 6;
+            this.botao_7.TabStop = false;
+            this.botao_7.Text = "7";
+            this.botao_7.UseVisualStyleBackColor = true;
+            this.botao_7.Click += new System.EventHandler(this.botao_7_Click);
+            // 
+            // botao_8
+            // 
+            this.botao_8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao_8.Location = new System.Drawing.Point(133, 126);
+            this.botao_8.Name = "botao_8";
+            this.botao_8.Size = new System.Drawing.Size(75, 23);
+            this.botao_8.TabIndex = 7;
+            this.botao_8.TabStop = false;
+            this.botao_8.Text = "8";
+            this.botao_8.UseVisualStyleBackColor = true;
+            this.botao_8.Click += new System.EventHandler(this.botao_8_Click);
+            // 
+            // botao_9
+            // 
+            this.botao_9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao_9.Location = new System.Drawing.Point(205, 126);
+            this.botao_9.Name = "botao_9";
+            this.botao_9.Size = new System.Drawing.Size(75, 23);
+            this.botao_9.TabIndex = 8;
+            this.botao_9.TabStop = false;
+            this.botao_9.Text = "9";
+            this.botao_9.UseVisualStyleBackColor = true;
+            this.botao_9.Click += new System.EventHandler(this.botao_9_Click);
+            // 
+            // botao_0
+            // 
+            this.botao_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao_0.Location = new System.Drawing.Point(133, 186);
+            this.botao_0.Name = "botao_0";
+            this.botao_0.Size = new System.Drawing.Size(75, 23);
+            this.botao_0.TabIndex = 9;
+            this.botao_0.TabStop = false;
+            this.botao_0.Text = "0";
+            this.botao_0.UseVisualStyleBackColor = true;
+            this.botao_0.Click += new System.EventHandler(this.botao_0_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(440, 50);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // botao_apagar
+            // 
+            this.botao_apagar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao_apagar.Location = new System.Drawing.Point(205, 106);
+            this.botao_apagar.Name = "botao_apagar";
+            this.botao_apagar.Size = new System.Drawing.Size(75, 23);
+            this.botao_apagar.TabIndex = 11;
+            this.botao_apagar.TabStop = false;
+            this.botao_apagar.Text = "←";
+            this.botao_apagar.UseVisualStyleBackColor = true;
+            this.botao_apagar.Click += new System.EventHandler(this.botao_apagar_Click);
+            // 
+            // botao_limpar
+            // 
+            this.botao_limpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao_limpar.Location = new System.Drawing.Point(133, 106);
+            this.botao_limpar.Name = "botao_limpar";
+            this.botao_limpar.Size = new System.Drawing.Size(75, 23);
+            this.botao_limpar.TabIndex = 12;
+            this.botao_limpar.TabStop = false;
+            this.botao_limpar.Text = "C";
+            this.botao_limpar.UseVisualStyleBackColor = true;
+            this.botao_limpar.Click += new System.EventHandler(this.botao_limpar_Click);
+            // 
+            // botao_virgula
+            // 
+            this.botao_virgula.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao_virgula.Location = new System.Drawing.Point(205, 186);
+            this.botao_virgula.Name = "botao_virgula";
+            this.botao_virgula.Size = new System.Drawing.Size(75, 23);
+            this.botao_virgula.TabIndex = 13;
+            this.botao_virgula.TabStop = false;
+            this.botao_virgula.Text = ",";
+            this.botao_virgula.UseVisualStyleBackColor = true;
+            this.botao_virgula.Click += new System.EventHandler(this.botao_virgula_Click);
+            // 
+            // botao_somar
+            // 
+            this.botao_somar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao_somar.Location = new System.Drawing.Point(277, 186);
+            this.botao_somar.Name = "botao_somar";
+            this.botao_somar.Size = new System.Drawing.Size(75, 23);
+            this.botao_somar.TabIndex = 14;
+            this.botao_somar.TabStop = false;
+            this.botao_somar.Text = "+";
+            this.botao_somar.UseVisualStyleBackColor = true;
+            this.botao_somar.Click += new System.EventHandler(this.botao_somar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 225);
+            this.Controls.Add(this.botao_somar);
+            this.Controls.Add(this.botao_virgula);
+            this.Controls.Add(this.botao_limpar);
+            this.Controls.Add(this.botao_apagar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.botao_0);
+            this.Controls.Add(this.botao_9);
+            this.Controls.Add(this.botao_8);
+            this.Controls.Add(this.botao_7);
+            this.Controls.Add(this.botao_6);
+            this.Controls.Add(this.botao_5);
+            this.Controls.Add(this.botao_4);
+            this.Controls.Add(this.botao_3);
+            this.Controls.Add(this.botao_2);
+            this.Controls.Add(this.botao_1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PixelCalc";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botao_1;
+        private System.Windows.Forms.Button botao_2;
+        private System.Windows.Forms.Button botao_3;
+        private System.Windows.Forms.Button botao_4;
+        private System.Windows.Forms.Button botao_5;
+        private System.Windows.Forms.Button botao_6;
+        private System.Windows.Forms.Button botao_7;
+        private System.Windows.Forms.Button botao_8;
+        private System.Windows.Forms.Button botao_9;
+        private System.Windows.Forms.Button botao_0;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button botao_apagar;
+        private System.Windows.Forms.Button botao_limpar;
+        private System.Windows.Forms.Button botao_virgula;
+        private System.Windows.Forms.Button botao_somar;
+    }
+}
+
---
+++ Vitor/PixelCalc/PixelCalc/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PixelCalc
+{
+    public partial class Form1 : Form
+    { 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double num1 = 0.0, num2 = 0.0,resultado=0.0;
+        int flag_num = 1;
+        private void completar_textbox(string n)
+        {
+            int tam = textBox1.TextLength;
+            string str = textBox1.Text;
+            if ((tam==1)&&(str=="0"))
+                textBox1.Text = "";
+
+            textBox1.Text = textBox1.Text + n;
+        }
+
+        private void botao_0_Click(object sender, EventArgs e)
+        {
+            completar_textbox("0");
+        }
+
+        private void botao_1_Click(object sender, EventArgs e)
+        {
+            completar_textbox("1");
+        }
+
+        private void botao_2_Click(object sender, EventArgs e)
+        {
+            completar_textbox("2");
+        }
+
+        private void botao_3_Click(object sender, EventArgs e)
+        {
+            completar_textbox("3");
+        }
+
+        private void botao_4_Click(object sender, EventArgs e)
+        {
+            completar_textbox("4");
+        }
+
+        private void botao_5_Click(object sender, EventArgs e)
+        {
+            completar_textbox("5");
+        }
+
+        private void botao_6_Click(object sender, EventArgs e)
+        {
+            completar_textbox("6");
+        }
+
+        private void botao_7_Click(object sender, EventArgs e)
+        {
+            completar_textbox("7");
+        }
+
+        private void botao_8_Click(object sender, EventArgs e)
+        {
+            completar_textbox("8");
+        }
+
+        private void botao_9_Click(object sender, EventArgs e)
+        {
+            completar_textbox("9");
+        }
+
+        private void botao_limpar_Click(object sender, EventArgs e)
+        {
+            num1 = 0;
+            num2 = 0;
+            textBox1.Text = "0";
+        }
+
+        private void botao_apagar_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength != 0)
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.TextLength - 1);
+
+                if (textBox1.TextLength == 0)
+                    textBox1.Text = "0";
+            }
+            else
+                textBox1.Text = "0";
+        }
+
+        bool existe_virgula = false;
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int carac = Convert.ToInt16(e.KeyChar);
+            switch (carac)
+            {
+                //FAZER BOTÃO ESC PARA O CLEAR
+                case 8:
+                    if (textBox1.TextLength != 0)
+                    {
+                        if (textBox1.Text[textBox1.TextLength - 1] == ',')
+                            existe_virgula = false;
+
+                        textBox1.Text = textBox1.Text.Remove(textBox1.TextLength - 1);
+
+                        if (textBox1.TextLength == 0)
+                            textBox1.Text = "0";
+                    }
+                    else
+                        textBox1.Text = "0";
+                    break;
+                case 43:
+                    if (flag_num == 1)
+                    {
+                        num1 = Convert.ToDouble(textBox1.Text);
+                        textBox1.Text = "";
+                        flag_num = 2;
+                    }
+                    else
+                    {
+                        num2 = num1 + Convert.ToDouble(textBox1.Text);
+                        flag_num = 1;
+                    }
+                    break;
+                case 44:
+                    if (!existe_virgula)
+                    {
+                        int tam = textBox1.TextLength;
+                        string str = textBox1.Text;
+                        if ((tam == 1) && (str == "0"))
+                            completar_textbox("0,");
+                        else
+                            completar_textbox(",");
+                        existe_virgula = true;
+                    }
+                    break;
+                case 46:
+                    if (!existe_virgula)
+                    {
+                        int tam = textBox1.TextLength;
+                        string str = textBox1.Text;
+                        if ((tam == 1) && (str == "0"))
+                            completar_textbox("0,");
+                        else
+                            completar_textbox(",");
+                        existe_virgula = true;
+                    }
+                    break;
+                case 48:
+                    completar_textbox("0");
+                    break;
+                case 49:
+                    completar_textbox("1");
+                    break;
+                case 50:
+                    completar_textbox("2");
+                    break;
+                case 51:
+                    completar_textbox("3");
+                    break;
+                case 52:
+                    completar_textbox("4");
+                    break;
+                case 53:
+                    completar_textbox("5");
+                    break;
+                case 54:
+                    completar_textbox("6");
+                    break;
+                case 55:
+                    completar_textbox("7");
+                    break;
+                case 56:
+                    completar_textbox("8");
+                    break;
+                case 57:
+                    completar_textbox("9");
+                    break;
+            }
+        }
+
+        private void botao_virgula_Click(object sender, EventArgs e)
+        {
+            if (!existe_virgula)
+            {
+                int tam = textBox1.TextLength;
+                string str = textBox1.Text;
+                if ((tam == 1) && (str == "0"))
+                    completar_textbox("0,");
+                else
+                    completar_textbox(",");
+                existe_virgula = true;
+            }        
+        }
+
+        private void botao_somar_Click(object sender, EventArgs e)
+        {
+            if (flag_num == 1)
+            {
+                num1 = Convert.ToDouble(textBox1.Text);
+                textBox1.Text = "";
+                flag_num = 2;
+            }
+            else
+            {
+                num2 = num1+Convert.ToDouble(textBox1.Text);
+                flag_num = 1;
+            }
+        }
+
+        private double somar()
+        {
+            return num1 + num2;
+        }
+    }
+}
+
